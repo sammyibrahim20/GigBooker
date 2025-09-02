366f6b838c8e04ede47565cfdd997bcb189d77ea
Sammy Ibrahim <sammyibr@buffalo.edu>
2025-09-02 11:42:46 -0400

    Test Commit

---
+++ src/main/java/com/sammyibrahim20/gigbooker/GigBookerApplication.java
@@ -0,0 +1,13 @@
+package com.sammyibrahim20.gigbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigBookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GigBookerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sammyibrahim20/gigbooker/GigBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sammyibrahim20.gigbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigBookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
