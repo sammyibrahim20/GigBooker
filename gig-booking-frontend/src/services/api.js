0153689014318591a80e976a64d761e8e1fbf688
Sammy Ibrahim <sammyibr@buffalo.edu>
2025-11-18 03:59:05 -0500

    modified api.js to point to render webpage

--- gig-booking-frontend/src/services/api.js
+++ gig-booking-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // backend root
+  baseURL: "https://gigbooker-docker.onrender.com", // backend root
   timeout: 15000,
 });
 
